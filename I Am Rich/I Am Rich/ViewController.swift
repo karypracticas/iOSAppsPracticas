5ffc3de490da8fb9fa4d6add9cf6ca5009ae846e
Kary Rguez <practicasgithub@gmail.com>
2020-10-26 12:53:00 -0600

    I Am Rich - Creacion proyecto

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kary on 26/10/20.
+//  Copyright © 2020 Kary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
